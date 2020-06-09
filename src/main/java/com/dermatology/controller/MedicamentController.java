60a2ba10d6e2358ddfd456e990658e525ccd5a65
jovica.josipovic97@gmail.com <jovica.josipovic97@gmail.com>
2020-06-09 13:06:21 +0200

    Adjust for front

--- src/main/java/com/dermatology/controller/MedicamentController.java
+++ src/main/java/com/dermatology/controller/MedicamentController.java
@@ -1,24 +1,78 @@
 package com.dermatology.controller;
 
-import com.dermatology.dto.DiseaseDto;
+import com.dermatology.cbr.CbrApplication;
+import com.dermatology.dto.ExamDTO;
 import com.dermatology.dto.MedicamentDto;
+import com.dermatology.model.Exam;
+import com.dermatology.model.Patient;
+import com.dermatology.model.PatientDescription;
+import com.dermatology.service.interfaces.ExamService;
+import com.dermatology.service.interfaces.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ucm.gaia.jcolibri.cbrcore.CBRQuery;
+import ucm.gaia.jcolibri.method.retrieve.RetrievalResult;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "medicament")
 public class MedicamentController {
 
+    @Autowired
+    private ExamService examService;
+
+    @Autowired
+    private PatientService patientService;
 
     @PostMapping("/predict/{patientId}")
     public String predict(Model model, @Valid @ModelAttribute("medicationDto") MedicamentDto medicamentDto, @PathVariable String patientId){
-        System.out.println(medicamentDto.getDisease());
+        try {
+            List<Exam> examCases = this.examService.findAll();
+
+            CbrApplication application = new CbrApplication(examCases);
+
+            application.configure();
+            application.preCycle();
+            CBRQuery query = new CBRQuery();
+
+            Patient p = this.patientService.find(Long.parseLong(patientId));
+
+            PatientDescription patientDescription = new PatientDescription();
+            patientDescription.setAge(p.getAge());
+            patientDescription.setGender(p.getGender());
+
+            patientDescription.setDisease(medicamentDto.getDisease());
+
+            query.setDescription(patientDescription);
+
+            application.cycle(query);
+            application.preCycle();
+
+            Collection<RetrievalResult> cases = application.predict();
+            List<ExamDTO> foundCasesDTO = new ArrayList<>();
+            int i = 1;
+            for (RetrievalResult res : cases) {
+                String[] s = res.get_case().getDescription().toString().split("caseId=");
+                Long id = Long.parseLong(s[1].split("}")[0]);
+                Exam e2 = this.examService.find(id);
+                ExamDTO dto = new ExamDTO(e2, res.getEval());
+                foundCasesDTO.add(dto);
+                System.out.println(i + " . pronadjeni slucaj je : " + dto);
+            }
+        } catch (Exception e) {
+            //null
+            return "bilo sta";
+        }
+        //foundCasesDTO
         return "hello";
     }
 
